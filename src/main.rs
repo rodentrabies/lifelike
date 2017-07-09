816a8aaefd8f9bf89e1a6ed1be788a80130c1507
whythat <whythat@protonmail.com>
2017-07-09 04:30:00 +0300

    Implement FromStr for Ruleset

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,16 @@
 // Life-like cellular automaton
 extern crate rand;
+extern crate regex;
 extern crate termion;
 
 mod grid;
 mod ruleset;
 mod ui;
 
+use std::str::FromStr;
 use ruleset::Ruleset;
 use ui::mainloop;
 
 fn main() {
-    mainloop(30, 30, Ruleset{born: vec![3], survive: vec![2, 3]});
+    mainloop(30, 30, Ruleset::from_str("B3/S23").unwrap());
 }
-
--- src/ruleset.rs
+++ src/ruleset.rs
@@ -1,6 +1,30 @@
-/// Ruleset type
+use regex::Regex;
+use std::str::FromStr;
+
+
+#[derive(Debug)]
+pub struct ParseRulesetError;
 
 pub struct Ruleset {
     pub born: Vec<i8>,
     pub survive: Vec<i8>,
+}
+
+impl FromStr for Ruleset {
+    type Err = ParseRulesetError;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = Regex::new(r"(b|B)(?P<b>[1-9]+)/(s|S)(?P<s>[1-9]+)").unwrap();
+        let parse_digits = |s: &str| {
+            s.chars()
+                .map(|c| c.to_digit(10).unwrap() as i8)
+                .collect::<Vec<i8>>()
+        };
+        match re.captures(s) {
+            Some(cap) => Ok(Ruleset {
+                born: parse_digits(&cap["b"]),
+                survive: parse_digits(&cap["s"]),
+            }),
+            _ => Err(ParseRulesetError),
+        }
+    }
 }
