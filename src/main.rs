c120de24f341067c76e34662b40daa57ddaee995
whythat <whythat@protonmail.com>
2017-07-11 20:44:42 +0300

    Add Ruleset command line parameter

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // Life-like cellular automaton
+extern crate getopts;
 extern crate rand;
 extern crate regex;
 extern crate termion;
@@ -7,10 +8,26 @@ mod grid;
 mod ruleset;
 mod ui;
 
-use std::str::FromStr;
+use getopts::Options;
 use ruleset::Ruleset;
+use std::env;
+use std::str::FromStr;
 use ui::mainloop;
 
 fn main() {
-    mainloop(30, 30, Ruleset::from_str("B3/S23").unwrap());
+    let args = env::args().collect::<Vec<String>>();
+    let mut opts = Options::new();
+    opts.optopt("r", "ruleset", "Ruleset (default: 'B3/S23')", "RULES");
+    opts.optflag("h", "help", "Show help menu");
+    let matches = opts.parse(&args).unwrap();
+    let ruleset = match matches.opt_str("r") {
+        Some(s) => s,
+        None => String::from("B3/S23"),
+    };
+    if matches.opt_present("h") || !matches.free.is_empty() {
+        let brief = format!("Usage: {} [options]", args[0].clone());
+        print!("{}", opts.usage(&brief));
+    } else {
+        mainloop(30, 30, Ruleset::from_str(&ruleset).unwrap());
+    }
 }
